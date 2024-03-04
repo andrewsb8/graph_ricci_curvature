88f139fa0dd0099c1a1b404f37e2e30fdcaf9e25
brian <b9andrews@gmail.com>
2024-03-04 12:58:15 -0500

    moving methods back to Ollivier class bc they are not involed in Forman curvature and cleaning up methods

--- src/graph_ricci_curvature/ollivier_ricci_curvature.py
+++ src/graph_ricci_curvature/ollivier_ricci_curvature.py
@@ -1,8 +1,26 @@
 import networkx as nx
 import numpy as np
 import ot
-import ricci_curvature
+from ricci_curvature import RicciCurvature
 
 class OllivierRicciCurvature(RicciCurvature):
     def __init__(self):
         pass
+
+    def _get_num_neighbors(self, graph, node):
+        return len(list(graph.neighbors(node)))
+
+    def _neighborhood_mass_distributions(node, num_neighbors, alpha):
+        distributions = [(1 - alpha)/num_neighbors] * num_neighbors
+        distributions = np.array(distributions.append(alpha))
+        return distributions
+
+    def _get_shortest_path_matrix(graph, source_neighborhood, target_neighborhood):
+        #find shortest distance between every node in source neighborhood (attached to source node by one edge) and every node in target neighborhood
+        d = []
+        for source in source_neighborhood:
+            tmp = []
+            for target in target_neighborhood:
+                tmp.append(nx.shortest_path_length(graph, source, target))
+            d.append(tmp)
+        return np.array(d)
--- src/graph_ricci_curvature/ricci_curvature.py
+++ src/graph_ricci_curvature/ricci_curvature.py
@@ -1,22 +1,7 @@
 from abc import ABC, abstractmethod
+import networkx as nx
+import numpy as np
 
 class RicciCurvature(ABC):
     def __init__(self):
         pass
-
-    def _get_neighbors_and_distributions(graph, node, alpha):
-        neighbors = list(graph.neighbors(node))
-        distributions = [(1 - alpha)/len(neighbors)] * len(neighbors)
-        neighbors = neighbors + [node]
-        distributions = distributions + [alpha]
-        return np.array(neighbors), np.array(distributions)
-
-    def _get_shortest_path_matrix(graph, source_neighborhood, target_neighborhood):
-        #find shortest distance between every node in source neighborhood (attached to source node by one edge) and every node in target neighborhood
-        d = []
-        for source in source_neighborhood:
-            tmp = []
-            for target in target_neighborhood:
-                tmp.append(nx.shortest_path_length(graph, source, target))
-            d.append(tmp)
-        return np.array(d)
