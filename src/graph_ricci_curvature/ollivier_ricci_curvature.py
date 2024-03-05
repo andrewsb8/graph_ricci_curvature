e97cb49ee9a4961167f28bf5be7d954ee0c2886c
brian <b9andrews@gmail.com>
2024-03-05 13:02:28 -0500

    added test for expected curvature value, formatted repo

--- src/graph_ricci_curvature/ollivier_ricci_curvature.py
+++ src/graph_ricci_curvature/ollivier_ricci_curvature.py
@@ -3,36 +3,48 @@ import numpy as np
 import ot
 from src.graph_ricci_curvature.ricci_curvature import RicciCurvature
 
+
 class OllivierRicciCurvature(RicciCurvature):
     def __init__(self, G: nx.Graph, weight_key="weight"):
         super().__init__(G, weight_key)
 
     def _calculate_edge_curvature(self, source_node, target_node):
-        source_neighbors = self._get_neighbors(source_node)
-        source_dist = self._neighborhood_mass_distribution(len(source_neighbors))
-        target_neighbors = self._get_neighbors(target_node)
-        target_dist = self._neighborhood_mass_distribution(len(target_neighbors))
-        short_path_matrix = self._get_shortest_path_matrix(source_neighbors, target_neighbors)
-        return 1 - (ot.emd2(source_dist, target_dist, short_path_matrix)/self.G.edges[source_node, target_node][self.weight_key])
+        source_neighbors, source_dist = self._neighborhood_mass_distribution(
+            source_node
+        )
+        target_neighbors, target_dist = self._neighborhood_mass_distribution(
+            target_node
+        )
+        short_path_matrix = self._get_shortest_path_matrix(
+            source_neighbors, target_neighbors
+        )
+        return 1 - (
+            ot.emd2(source_dist, target_dist, short_path_matrix)
+            / self.G.edges[source_node, target_node][self.weight_key]
+        )
 
     def _get_neighbors(self, node):
-        return list(self.G.neighbors(node)).append(node)
+        return list(self.G.neighbors(node))
 
-    def _neighborhood_mass_distribution(self, num_neighbors, alpha=0.5):
-        #TO DO: add case for node with no neighbors
-        #TO DO: make sure neighbors is not zero somewhere
-        distribution = [((1 - alpha)/num_neighbors) for neighbor in num_neighbors]
-        distribution = np.array(distributions.append(alpha))
-        return distribution
+    def _neighborhood_mass_distribution(self, node, alpha=0.5):
+        # TO DO: add case for node with no neighbors
+        # TO DO: make sure neighbors is not zero somewhere
+        node_neighbors = self._get_neighbors(node)
+        distribution = [
+            ((1 - alpha) / len(node_neighbors)) for neighbor in node_neighbors
+        ]
+        node_neighbors = np.array(node_neighbors + [node])
+        distribution = np.array(distribution + [alpha])
+        return node_neighbors, distribution
 
     def _get_shortest_path_matrix(self, source_neighborhood, target_neighborhood):
-        #find shortest distance between every node in source neighborhood (attached to source node by one edge) and every node in target neighborhood
-        """d = []
-        for source in source_neighborhood:
-            tmp = []
-            for target in target_neighborhood:
-                tmp.append(nx.shortest_path_length(graph, source, target))
-            d.append(tmp)
-        return np.array(d)"""
-
-        return np.array([ [nx.shortest_path_length(graph, source, target) for target in target_neighborhood] for source in source_neighborhood ])
+        # find shortest distance between every node in source neighborhood (attached to source node by one edge) and every node in target neighborhood
+        return np.array(
+            [
+                [
+                    nx.shortest_path_length(self.G, source, target)
+                    for target in target_neighborhood
+                ]
+                for source in source_neighborhood
+            ]
+        )
--- src/graph_ricci_curvature/ricci_curvature.py
+++ src/graph_ricci_curvature/ricci_curvature.py
@@ -4,6 +4,7 @@ import numpy as np
 import sys
 from src.exceptions.exceptions import NotImplementedError
 
+
 class RicciCurvature(ABC):
     def __init__(self, G: nx.Graph, weight_key):
         self.G = G.copy()
@@ -16,13 +17,17 @@ class RicciCurvature(ABC):
 
     def _check_edge_weights(self):
         if not nx.get_edge_attributes(self.G, self.weight_key):
-            sys.stderr.write("No edge weights detected, setting edge weights to one with weight_key = weight\n")
+            sys.stderr.write(
+                "No edge weights detected, setting edge weights to one with weight_key = weight\n"
+            )
             self._set_edge_weights()
 
     def _set_edge_weights(self):
-        for (i,j) in self.G.edges():
+        for i, j in self.G.edges():
             self.G[i][j][self.weight_key] = 1.0
 
     def _check_directed(self):
         if self.G.is_directed():
-            raise NotImplementedError("Directed Graphs are not implemented. Set your graph to undirected with self.G.to_undirected().")
+            raise NotImplementedError(
+                "Directed Graphs are not implemented. Set your graph to undirected with self.G.to_undirected()."
+            )
