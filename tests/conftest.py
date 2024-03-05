7cd861b03b76c15be015548d57ca0f35af1c4955
brian <b9andrews@gmail.com>
2024-03-05 13:29:23 -0500

    added fixture for tests

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+import networkx as nx
+
+@pytest.fixture
+def simple_graph():
+    """Mock optimizer"""
+    G = nx.Graph()
+    G.add_nodes_from([1, 2, 3])
+    G.add_edges_from([(1, 2), (1, 3)])
+    return G
--- tests/test_cases/test_ollivier_ricci_curvature.py
+++ tests/test_cases/test_ollivier_ricci_curvature.py
@@ -1,21 +1,12 @@
 import pytest
-import networkx as nx
 from src.graph_ricci_curvature.ricci_curvature import RicciCurvature
 from src.graph_ricci_curvature.ollivier_ricci_curvature import OllivierRicciCurvature
 
 
-def test_calculate_edge_curvature():
-    G = nx.Graph()
-    G.add_nodes_from([1, 2, 3])
-    G.add_edges_from([(1, 2), (1, 3)])
-
-    obj = OllivierRicciCurvature(G)
+def test_calculate_edge_curvature(simple_graph):
+    obj = OllivierRicciCurvature(simple_graph)
     assert obj._calculate_edge_curvature(1, 2) == 0.5
 
-def test_tensor_symmetry():
-    G = nx.Graph()
-    G.add_nodes_from([1, 2, 3])
-    G.add_edges_from([(1, 2), (1, 3)])
-
-    obj = OllivierRicciCurvature(G)
+def test_tensor_symmetry(simple_graph):
+    obj = OllivierRicciCurvature(simple_graph)
     assert obj._calculate_edge_curvature(1, 2) == obj._calculate_edge_curvature(2, 1)
