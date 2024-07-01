05d10cc5e8c81aa88b6beb4542dbaaefb88de0f0
bandrews <b9andrews@gmail.com>
2024-07-01 11:29:14 -0400

    add version

--- graph_ricci_curvature/__init__.py
+++ graph_ricci_curvature/__init__.py
@@ -1,4 +1,4 @@
 __author__ = "Brian Andrews"
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 __github__ = "https://github.com/andrewsb8/graph-ricci-curvature"
 __manual__ = "https://github.com/andrewsb8/graph_ricci_curvature/blob/docs/docs/_build/latex/graph_ricci_curvature.pdf"
